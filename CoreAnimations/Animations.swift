7d6a7cb48da3d413071defb540cf12f375cb5fa6
lfcj <fernanda16400@gmail.com>
2021-11-08 21:52:07 +0100

    Remove types of animations

--- CoreAnimations/Animations.swift
+++ CoreAnimations/Animations.swift
@@ -7,26 +7,19 @@ struct Animations {
     typealias Values = [Any]
     typealias KeyTimes = [NSNumber]
 
-    enum TheType {
-        case keyFrame
-        case basic
-    }
-
     struct Properties {
         let keyPath: KeyPath
         let values: Values
         let keyTimes: KeyTimes
         let duration: CFTimeInterval
         let repetitions: Float
-        let type: TheType
 
-        init(_ keyPath: KeyPath, _ values: Values, _ keyTimes: [NSNumber] = [0, 0.5, 1], _ duration: CFTimeInterval = 0.3, _ repetitions: Float = 5, type: TheType = .keyFrame) {
+        init(_ keyPath: KeyPath, _ values: Values, _ keyTimes: [NSNumber] = [0, 0.5, 1], _ duration: CFTimeInterval = 0.3, _ repetitions: Float = 5) {
             self.keyPath = keyPath
             self.values = values
             self.keyTimes = keyTimes
             self.duration = duration
             self.repetitions = repetitions
-            self.type = type
         }
     }
 
@@ -36,7 +29,7 @@ struct Animations {
         Properties("anchorPoint", [CGPoint(x: 0, y: 0), CGPoint(x: 0.1, y: 0.1), CGPoint(x: 0, y: 0)]),
         Properties("backgroundColor", [UIColor.systemRed.cgColor, UIColor.systemYellow.cgColor, UIColor.systemGreen.cgColor]),
         Properties("backgroundFilters", [/*todo*/]),
-        Properties("borderColor", [UIColor.systemRed.cgColor, UIColor.systemYellow.cgColor, UIColor.systemGreen.cgColor], type: .basic),
+        Properties("borderColor", [UIColor.systemRed.cgColor, UIColor.systemYellow.cgColor, UIColor.systemGreen.cgColor]),
         Properties("transform.scale", [1.0, 1.2, 1.0]),
         Properties("transform.rotation.y", [1.0, 1.5, 1.0]),
         Properties("transform.rotation.x", [1.0, 1.5, 1.0]),
@@ -55,13 +48,7 @@ struct Animations {
             return nil
         }
 
-        let animationProperties = properties[index]
-        switch animationProperties.type {
-        case .keyFrame:
-            return makeAnimation(keyPath: animationProperties.keyPath, properties: animationProperties)
-        case .basic:
-            return makeBasicAnimation(keyPath: animationProperties.keyPath, properties: animationProperties)
-        }
+        return makeAnimation(keyPath: animationProperties.keyPath, properties: properties[index])
     }
 
     func makeAnimation(keyPath: KeyPath, properties: Properties) -> CAKeyframeAnimation {
